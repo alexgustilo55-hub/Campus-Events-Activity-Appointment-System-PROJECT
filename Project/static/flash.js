2a45a940f0725290a0340bd1be54f42ed0d48527
Alex Reyes Gustilo <alexgustilo55@gmail.com>
2025-09-14 23:55:03 +0800

    Add files via upload

--- Project/static/flash.js
+++ Project/static/flash.js
@@ -7,27 +7,7 @@ function toggleProfile() {
 
 /* ----------- Flash message effect ------------- */
 
-document.addEventListener('DOMContentLoaded', function() {
-    let flashmessage = document.getElementById('flash-message');
-    if (flashmessage){
 
-        setTimeout(() => {
-            flashmessage.style.opacity = '0';;
-            flashmessage.style.transition = 'opacity 1s ease';
-            setTimeout(() => {
-                flashmessage.style.display = 'none';
-            },1000)
-        }, 3000)
-    }
-});
 
 /*--------------login page effects------------------*/
-document.addEventListener('DOMContentLoaded', function() {
-    let loginBox = document.querySelector('.login-box');
-    if (loginBox) {
-        setTimeout(() => {
-            loginBox.classList.add('show');
-        },300);
-    }
-});
 

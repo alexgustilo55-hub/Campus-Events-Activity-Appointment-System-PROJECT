6f2a32e2f79f748afa87bd9106c87ffcd75332d7
Alex Reyes Gustilo <alexgustilo55@gmail.com>
2025-09-29 20:06:39 +0800

    Add files via upload

--- Project/static/flash.js
+++ Project/static/flash.js
@@ -1,10 +1,11 @@
-/*------------ Toggle profile  ------------*/
+/*---------------------------------- Toggle profile  -------------------------*/
 
 function toggleProfile() {
             var card = document.getElementById("profileCard");
             card.style.display = (card.style.display === "block") ? "none" : "block";
         }
 
+/*---------------------------------- FLASH MESSAGE  -------------------------*/
 
 
 

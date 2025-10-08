4adffda6487dad3bbf0e49b40e9ec7b3d51f8448
Alex Reyes Gustilo <alexgustilo55@gmail.com>
2025-10-08 19:33:09 +0800

    Add files via upload

---
+++ project2/static/flash.js
@@ -0,0 +1,26 @@
+/*---------------------------------- Toggle profile  -------------------------*/
+
+function toggleProfile() {
+            var card = document.getElementById("profileCard");
+            card.style.display = (card.style.display === "block") ? "none" : "block";
+        }
+
+/*----------------------------------  -------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    const roleSelect = document.getElementById('role');
+    const organizerTypeDiv = document.getElementById('organizerType');
+
+    roleSelect.addEventListener('change', function() {
+        if (this.value === 'organizer') {
+            organizerTypeDiv.style.display = 'block';
+        } else {
+            organizerTypeDiv.style.display = 'none';
+        }
+    });
+});
+
+
+
+
+
+
